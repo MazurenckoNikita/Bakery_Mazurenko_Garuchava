923a726c68d5966f3af8bb6d7284e78b15e3f9cf
MazurenckoNikita <alenksze@gmail.com>
2023-03-27 14:31:47 +0300

    27.03

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Runtime.Remoting.Contexts;
+using System.Windows;
+using Garuchava_Mazurenko_Bakery.DB;
+using Garuchava_Mazurenko_Bakery.Windows.ClientWindows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using static Garuchava_Mazurenko_Bakery.ClassHelper.EFClass;
+using Garuchava_Mazurenko_Bakery.Windows;
+
+namespace Garuchava_Mazurenko_Bakery
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var userAuth = ContextDB.UserAccount.ToList()
+            .Where(i => i.Email == loginTb.Text && i.Password == PassTB.Password)
+            .FirstOrDefault();
+
+            if (loginTb.Text.Length > 0) 
+            {
+                if (PassTB.Password.Length > 0)       
+                {
+
+                    if (string.IsNullOrWhiteSpace(loginTb.Text))
+                    {
+                        MessageBox.Show("Пользователя не найден");
+                    }
+                    else { MessageBox.Show("Пользователь авторизовался");
+                        ProductListWindow productListWindow = new ProductListWindow();
+                        productListWindow.Show();
+                        this.Close();
+                    }
+                    
+                    
+                    
+                }
+                else MessageBox.Show("Введите пароль"); 
+            }
+            else MessageBox.Show("Введите логин"); 
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            WindowRegistration windowRegistration = new WindowRegistration();
+            windowRegistration.Show();
+            this.Close();   
+        }
+
+        private void loginTb_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void loginTb_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (loginTb.Text == "Username")
+            {
+                loginTb.Clear();
+            }
+        }
+
+        private void loginTb_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(loginTb.Text))
+            {
+                loginTb.Text = "Username";
+            }
+        }
+
+        private void PassTB_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (PassTB.Password == "Password")
+            {
+                PassTB.Clear();
+            }
+        }
+
+        private void PassTB_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(PassTB.Password))
+            {
+                PassTB.Password = "Password";
+            }
+        }
+    }
+}
+
+
---
+++ Windows/ClientWindows/WindowRegistration.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static Garuchava_Mazurenko_Bakery.ClassHelper.EFClass;
+
+namespace Garuchava_Mazurenko_Bakery.Windows.ClientWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowRegistration.xaml
+    /// </summary>
+    public partial class WindowRegistration : Window
+    {
+        public WindowRegistration()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnReg_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(Tb_login.Content.ToString()))
+            {
+                MessageBox.Show("Пустой логин");
+                return;
+            }
+
+            ContextDB.UserAccount.Add(new DB.UserAccount()
+            {
+                Login = Tb_login.Content.ToString(),
+                Password = PassTB.Password,
+                Email = Tb_login.Content.ToString(),
+            });
+            ContextDB.SaveChanges();
+
+                MessageBox.Show("OK");
+
+            }
+        }
+    }
+
